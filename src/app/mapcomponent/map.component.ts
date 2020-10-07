8ed0f049f3b0c037d62ead5d736422ac181a9aa1
Steph Ugo <remo@neuf.fr>
2020-10-07 22:36:48 +0200

    Bug fixes in map component and threshold tuning.

--- src/app/mapcomponent/map.component.ts
+++ src/app/mapcomponent/map.component.ts
@@ -26,6 +26,8 @@ export class MapLayerComponent implements OnInit {
   private subscriber: any;
   private scalefactor = 0.5; // Currently we just have one computed scale factor (to be updated when we will display several locations)
   private selectedLocations: Array<ILocation> = [];
+  private locationLabels: Array<string> = [];
+  private orientationIndex = 0;
 
   constructor(private route: ActivatedRoute, private appStateService: AppSharedStateService) {
     this.appStateService.setRecords$.subscribe(
@@ -51,6 +53,8 @@ export class MapLayerComponent implements OnInit {
             }
           }
           this.selectedLocations = [];
+          this.locationLabels = [];
+          this.orientationIndex = 0;
           this.processSelectedRecords();
         }
       }
@@ -96,23 +100,44 @@ export class MapLayerComponent implements OnInit {
             const checkedLocation = this.processLocation(location);
             if (checkedLocation !== null) {
               this.selectedLocations.push(checkedLocation);
-              notif = {
-                id: record._id !== null ? record._id : 'null',
-                actionType: ActionType.ADD_UPDATE,
-                entity: {
+              if (this.locationLabels.includes(location.name)) {
+                notif = {
                   id: record._id !== null ? record._id : 'null',
-                  position: Cesium.Cartesian3.fromDegrees(checkedLocation.lon, checkedLocation.lat),
-                  name: location.name,
-                  scaleByDistance: new Cesium.NearFarScalar(1.5e2, this.scalefactor / 1.5, 1.0e4, this.scalefactor),
-                  image: this.backendServerURL + '/uploads/' + record.ImageFileName,
-                  label: {
-                    text: location.name,
-                    pixelOffset: new Cesium.Cartesian2(0, 130),
-                    translucencyByDistance: new Cesium.NearFarScalar(1.5e2, 1.0, 8.0e6, 0.0),
-                    font: '12px Helvetica' // Doesn't work...
+                  actionType: ActionType.ADD_UPDATE,
+                  entity: {
+                    id: record._id !== null ? record._id : 'null',
+                    position: Cesium.Cartesian3.fromDegrees(checkedLocation.lon, checkedLocation.lat),
+                    name: '',
+                    scaleByDistance: new Cesium.NearFarScalar(1.5e2, this.scalefactor / 1.5, 1.0e4, this.scalefactor),
+                    image: this.backendServerURL + '/uploads/' + record.ImageFileName,
+                    label: {
+                      text: '',
+                      pixelOffset: new Cesium.Cartesian2(0, 130),
+                      translucencyByDistance: new Cesium.NearFarScalar(1.5e2, 1.0, 8.0e6, 0.0),
+                      font: '12px Helvetica' // Doesn't work...
+                    }
                   }
-                }
-              };
+                };
+              } else {
+                this.locationLabels.push(location.name);
+                notif = {
+                  id: record._id !== null ? record._id : 'null',
+                  actionType: ActionType.ADD_UPDATE,
+                  entity: {
+                    id: record._id !== null ? record._id : 'null',
+                    position: Cesium.Cartesian3.fromDegrees(checkedLocation.lon, checkedLocation.lat),
+                    name: location.name,
+                    scaleByDistance: new Cesium.NearFarScalar(1.5e2, this.scalefactor / 1.5, 1.0e4, this.scalefactor),
+                    image: this.backendServerURL + '/uploads/' + record.ImageFileName,
+                    label: {
+                      text: location.name,
+                      pixelOffset: new Cesium.Cartesian2(0, 130),
+                      translucencyByDistance: new Cesium.NearFarScalar(1.5e2, 1.0, 8.0e6, 0.0),
+                      font: '12px Helvetica' // Doesn't work...
+                    }
+                  }
+                };
+              }
               this.subscriber.next(notif);
             }
           }
@@ -142,41 +167,40 @@ export class MapLayerComponent implements OnInit {
     const checkedLocation = obj as ILocation;
 
     // (Very) basic loop to avoid position conflicts
-    let orientationIndex = 0;
     for (let index = 0; index < this.selectedLocations.length; index++) {
       const alreadySelectedLocation = this.selectedLocations[index];
       const latDelta = checkedLocation.lat - alreadySelectedLocation.lat;
       const lonDelta = checkedLocation.lon - alreadySelectedLocation.lon;
-      if ((latDelta < 0.005) && (lonDelta < 0.005)) {
-          if (orientationIndex % 8 ===  0) {
-            checkedLocation.lat += (Math.floor(orientationIndex / 8) + 1) * 0.003;
+      if ((Math.abs(latDelta) < 0.0005) && (Math.abs(lonDelta) < 0.0005)) {
+          if (this.orientationIndex % 8 ===  0) {
+            checkedLocation.lat += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  1) {
-            checkedLocation.lat += (Math.floor(orientationIndex / 8) + 1) * 0.003;
-            checkedLocation.lon += (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  1) {
+            checkedLocation.lat += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
+            checkedLocation.lon += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  2) {
-            checkedLocation.lon += (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  2) {
+            checkedLocation.lon += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  3) {
-            checkedLocation.lat -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
-            checkedLocation.lon += (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  3) {
+            checkedLocation.lat -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
+            checkedLocation.lon += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  4) {
-            checkedLocation.lat -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  4) {
+            checkedLocation.lat -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  5) {
-            checkedLocation.lat -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
-            checkedLocation.lon -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  5) {
+            checkedLocation.lat -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
+            checkedLocation.lon -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  6) {
-            checkedLocation.lon -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  6) {
+            checkedLocation.lon -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          if (orientationIndex % 8 ===  7) {
-            checkedLocation.lat += (Math.floor(orientationIndex / 8) + 1) * 0.003;
-            checkedLocation.lon -= (Math.floor(orientationIndex / 8) + 1) * 0.003;
+          if (this.orientationIndex % 8 ===  7) {
+            checkedLocation.lat += (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
+            checkedLocation.lon -= (Math.floor(this.orientationIndex / 8) + 1) * 0.002;
           }
-          orientationIndex++;
+          this.orientationIndex++;
       }
     }
     return checkedLocation;
