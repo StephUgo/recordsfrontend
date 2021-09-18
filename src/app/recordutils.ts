da46f752f8d3754c80239b0b95350b31de76c60d
StephUgo <ugolini.stephane@neuf.fr>
2021-09-18 11:57:34 +0200

    Add new style.

--- src/app/recordutils.ts
+++ src/app/recordutils.ts
@@ -33,8 +33,9 @@ export class RecordUtils {
         { id: 19, name: 'Japanese', label: 'Japanese' },
         { id: 20, name: 'Electro', label: 'Electro' },
         { id: 21, name: 'Brit Funk', label: 'Brit Funk' },
-        { id: 22, name: 'Funky French', label: 'Funky French' }
-    ];
+        { id: 22, name: 'Funky French', label: 'Funky French' },
+        { id: 23, name: 'Pop', label: 'Pop' }
+   ];
 
     private formats: Array<string> = ['7"', '10"', '12"', 'LP', '2xLP', '3xLP', '2x7"', '3x7"'];
 
