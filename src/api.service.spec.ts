fe47f3b175842254471db3b3c70b9371edd4da65
StephUgo <ugolini.stephane@neuf.fr>
2023-11-25 10:19:19 +0100

    Fix unit tests

--- src/api.service.spec.ts
+++ src/api.service.spec.ts
@@ -2,15 +2,17 @@
 import { ApiService } from './api.service';
 import { Record } from './app/model/record';
 import { asyncData } from './testing/async-observable-helpers';
+import { RecordUtils } from './app/recordutils';
 
 describe('Service "ApiService" testing', () => {
     let httpClientSpy: { get: jasmine.Spy };
     let apiService: ApiService;
+    let recordutils: RecordUtils;
 
     beforeEach(() => {
         // Inject the http client spy for the test
         httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-        apiService = new ApiService(<any> httpClientSpy);
+        apiService = new ApiService(<any> httpClientSpy, recordutils);
     });
 
     it('Test getRecords().', () => {
@@ -19,7 +21,8 @@ describe('Service "ApiService" testing', () => {
                 Title: 'Idle Moments', Format: 'LP', Country: 'US',
                 Reference: 'MMBST-84154', Comments: 'Recorded At – Van Gelder Studio, Englewood Cliffs, NJ',
                 ImageFileName: 'GreenIdleMoments.jpg', Label: 'Blue Note - Music Matters',
-                Period: '60\'s', Year: 2014, keywords: ['Audiophile', 'Pressed at RTI']}];
+                Period: '60\'s', Year: 2014, keywords: ['Audiophile', 'Pressed at RTI'],
+                copyFrom: Record.prototype.copyFrom}];
 
         httpClientSpy.get.and.returnValue(asyncData(expectedRecords));
 
