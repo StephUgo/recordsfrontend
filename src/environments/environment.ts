df719ef298a83c441b1db191334b4187d7227118
StephUgo <ugolini.stephane@neuf.fr>
2023-11-26 12:19:10 +0100

    Fix environment indentation for lint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  backendURL: 'http://localhost',
-  backendPort: 3000
+    production: false,
+    backendURL: 'http://localhost',
+    backendPort: 3000
 };
 
 /*
