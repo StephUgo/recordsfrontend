3d55de50f5a473921f8be751f199872dab5b3387
StephUgo <ugolini.stephane@neuf.fr>
2023-12-28 12:08:49 +0100

    Update lint

--- src/app/cesium.service.spec.ts
+++ src/app/cesium.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { CesiumService } from './cesium.service';
 
 describe('CesiumService', () => {
-  let service: CesiumService;
+    let service: CesiumService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CesiumService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CesiumService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
